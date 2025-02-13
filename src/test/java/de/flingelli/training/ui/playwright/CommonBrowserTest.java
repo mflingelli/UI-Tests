a19359348f92d133640df47e0606d626398e06e1
Markus Flingelli <markus.flingelli@gmx.de>
2025-02-13 23:19:52 +0100

    Bug fix.

--- src/test/java/de/flingelli/training/ui/playwright/CommonBrowserTest.java
+++ src/test/java/de/flingelli/training/ui/playwright/CommonBrowserTest.java
@@ -3,6 +3,7 @@ package de.flingelli.training.ui.playwright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.apache.commons.lang3.SystemUtils;
 import org.testng.Assert;
 
 public class CommonBrowserTest {
@@ -39,6 +40,6 @@ public class CommonBrowserTest {
         page.locator("input[name=\"search\"]").click();
         page.locator("input[name=\"search\"]").fill("playwright");
         page.locator("input[name=\"search\"]").press("Enter");
-        Assert.assertTrue(page.url().startsWith("https://de.wikipedia.org"));
+        Assert.assertTrue(page.url().startsWith("https://en.wikipedia.org/wiki/Playwright"));
     }
 }
